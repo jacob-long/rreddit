bd3f6433c5b1eb64f51c2d1f6de744981c4df942
Jacob Long <4957401+jacob-long@users.noreply.github.com>
2020-12-03 13:32:49 -0500

    Remove non-existent "title" column in comments output

--- R/pushshift_io.R
+++ R/pushshift_io.R
@@ -68,7 +68,7 @@ get_reddit_posts <- function(subreddit = "all", n = 100, after = NULL,
   }
   out <- tryCatch(docall_rbind(x),
     error = function(e) x)
-  key_names <- c("id", "subreddit", "created_utc", "author", "title", "score",
+  key_names <- c("id", "subreddit", "created_utc", "author", "score",
                  "selftext", "full_link", "num_comments")
   names_order <- c(key_names, names(out) %not% key_names)
   out <- out[names_order]
