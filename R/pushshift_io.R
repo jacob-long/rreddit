487c9245e6a38130a5422b0586695cc6bf49b9d4
Jacob Long <4957401+jacob-long@users.noreply.github.com>
2020-12-03 11:47:53 -0500

    Drop empty requests
    Not doing this caused the function return a list with a mixture of DFs, one empty DF, and NULL when fewer than `n` results were found.

--- R/pushshift_io.R
+++ R/pushshift_io.R
@@ -56,6 +56,7 @@ get_r_reddit <- function(subreddit = "all", n = 100, after = NULL,
     j <- httr::content(r, as = "text", encoding = "UTF-8")
     j <- jsonlite::fromJSON(j)
     x[[i]] <- as_tbl(non_recs(j$data))
+    if (!"created_utc" %in% names(x[[i]])) x[[i]] <- NULL
     if (!"created_utc" %in% names(x[[i]])) break
     x[[i]] <- formate_createds(x[[i]])
     after <- x[[i]]$created_utc[nrow(x[[i]])]
